07f5c1c78899055ebf5c8397d5ebcc2a041ead4f
Shane Creedon <Shane.creedon3@mail.dcu.ie>
2019-04-14 16:38:05 +0100

    Shane File

--- shane.c
+++ shane.c
@@ -9,19 +9,21 @@ double cubes[40][4];
 // [0] is r [1] is g [2] is b
 double colors[40][3];
 // [0] is x_angle [1] is y_angle [2] is z_angle
-double rotations[40][3];
+float theta2[64], theta3[64];
 
 double spheres[40][2];
 GLUquadric* sphere_objects[40][1];
 
 int selected;
 int state;
+int shift = 0;
 
 GLfloat light_pos[] = {1.0, 2.0, 3.0, 1.0};
 double shape_state = 0; // Cube state // 1 is Sphere
 
 int count = 0;
 int sphere_count = 0;
+float theta = M_PI/4;
 
 enum {
   MENU_CUBE = 0,
@@ -30,42 +32,63 @@ enum {
   ROTATE,
 };
 
-void display() {
-    glClearColor(0.0,0.0,0.0,1.0);
-	glClear(GL_COLOR_BUFFER_BIT);
+void display(void) {
+	glClearColor(0.0,0.35,0.4,1.0);
 
+	glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH);
+	glEnable(GL_DEPTH_TEST);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	GLfloat  ambient[4] = {0.1, 1, 0.0, 1.0};
+	GLfloat  diffuse[4] = {0.2, 1, 0.0, 1.0};
+	GLfloat specular[4] = {0.2, 0.8, 0.7, 1.0};
+	GLfloat shininess = 32.0;
+	GLfloat normal[3] = {0.0,0.0,0.25};
+	GLfloat position[4] = {1.0,1.0,1.0,1.0};
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
+	gluLookAt(0.0,0.0,10.0,0.0,0.0,0.0,0.0,1.0,0.0);
 
-	gluLookAt(0.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0,0.0);
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+
+	position[1] = cos(theta); 
+	position[2] = sin(theta);
+
+	glLightfv(GL_LIGHT0, GL_POSITION, position);
+
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular);
+	glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
 
     // Draw Cubes
-    for (int i = 0; i < count; i++) {
-        glPushMatrix();
-            glColor3f(colors[i][0], colors[i][1], colors[i][2]);
-            glRotatef(rotations[i][0], 1, 0, 0);
-            glRotatef(rotations[i][1], 0, 1, 0);
-            glRotatef(rotations[i][2], 0, 0, 1);
-            glTranslatef(cubes[i][1], cubes[i][2], cubes[i][3]);
-            glutSolidCube(cubes[i][0]); 
-        glPopMatrix();
+    for (int i = count-1; i >= 0; i--) {
+        GLfloat x = cubes[i][1];
+		GLfloat y =	-cubes[i][2];
+        glRotatef(theta2[i], 0.0, 1.0, 0.0);
+        glRotatef(theta3[i], 1.0, 0.0, 0.0);
+        glTranslatef(x, y, 0.0);
+        glutSolidCube(cubes[i][0]); 
+        glTranslatef(-x, -y, 0.0);
     }
-	glFlush();
 
     // Draw Spheres
-    for (int i = 0; i < sphere_count; i++) {
-        glPushMatrix();
-            glColor3f(55, 55, 255);
-            glRotatef(0, 1, 0, 0);
-            glRotatef(0, 0, 1, 0);
-            glRotatef(0, 0, 0, 1);
-            glTranslatef(spheres[i][0], spheres[i][1], 0);
-            GLUquadric *quad = sphere_objects[i][0];
-            quad = gluNewQuadric();
-            gluSphere(quad, 0.15, 24, 24);
-        glPopMatrix();
+    for (int i = sphere_count-1; i >= 0; i--) {
+        GLfloat x = spheres[i][0];
+		GLfloat y =	-spheres[i][1];
+        glRotatef(theta2[i],0.0,1.0,0.0);
+        glRotatef(theta3[i],1.0,0.0,0.0);
+        glTranslatef(x, y, 0);
+        GLUquadric *quad = sphere_objects[i][0];
+        quad = gluNewQuadric();
+        gluSphere(quad, 0.15, 24, 24);
+        glTranslatef(-x, -y, 0);
     }
 	
+    glutSwapBuffers();
     glFlush();
 	return;
 
@@ -88,24 +111,17 @@ void mouseFunc(int button, int state, int x, int y) {
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
         float screen_x = (x - 250.0) / 250.0;
         float screen_y = (y - 250.0) / 250.0;
-        screen_y = screen_y * -1;
         printf("(x, y)  :  (%f, %f) \n", screen_x, screen_y);
 
         if (shape_state == 0) {
+            printf("Total Cubes: %d \n", count);
             cubes[count][0] = 0.5;
             cubes[count][1] = screen_x;
             cubes[count][2] = screen_y;
             cubes[count][3] = 0;
-
-            colors[count][0] = 55;
-            colors[count][1] = 55;
-            colors[count][2] = 255;
-
-            rotations[count][0] = 0;
-            rotations[count][1] = 0;
-            rotations[count][2] = 0;
             count++;
         }
+
         else if (shape_state == 1) {
             GLUquadric *quad;
             sphere_objects[sphere_count][0] = quad;
@@ -113,6 +129,10 @@ void mouseFunc(int button, int state, int x, int y) {
             spheres[sphere_count][1] = screen_y;
             sphere_count++;
         }
+
+        theta2[shift] = 0.0;
+		theta3[shift] = 0.0;
+        shift++;
     }
 
     glutPostRedisplay();
@@ -123,59 +143,6 @@ void keyboardFunc(unsigned char key, int x, int y) {
     // printf("%c", key);
     // printf("%d and %d", x, y);
     switch (key) {
-        case 'r':
-            state = ROTATE;
-            printf("Mode: ROTATE\n");
-            break;
-        case 'm':
-            state = MOVE;
-            printf("Mode: MOVE\n");
-            break;
-        case 'a':
-            if (state == MOVE) {
-                cubes[selected][1] -= 0.01;
-            }
-            else if (state == ROTATE) {
-                // rotate the cube
-                rotations[selected][1] -= 1;
-                if (rotations[selected][1] < -360) {
-                    rotations[selected][1] = 0;
-                }
-            }
-            break;
-        case 'd':
-            if (state == MOVE) {
-                cubes[selected][1] += 0.01;
-            }
-            else if (state == ROTATE) {
-                rotations[selected][1] += 1;
-                if (rotations[selected][1] < -360) {
-                    rotations[selected][1] = 0;
-                }
-            }
-            break;
-        case 'w':
-            if (state == MOVE) {
-                cubes[selected][2] += 0.01;
-            }
-            else if (state == ROTATE) {
-                rotations[selected][0] -= 1;
-                if (rotations[selected][0] < -360) {
-                    rotations[selected][0] = 0;
-                }
-            }
-            break;
-        case 's':
-            if (state == MOVE) {
-                cubes[selected][2] -= 0.01;
-            }
-            else if (state == ROTATE) {
-                rotations[selected][0] += 1;
-                if (rotations[selected][0] < -360) {
-                    rotations[selected][0] = 0;
-                }
-            }
-            break;
         case '1':
             colors[selected][0] = 255;
             colors[selected][1] = 0;
@@ -196,21 +163,48 @@ void keyboardFunc(unsigned char key, int x, int y) {
     glutPostRedisplay();
 }
 
+void rotate(int key, int x, int y) {
+    if(count > 0) {
+        if(key == GLUT_KEY_LEFT) theta2[count-1] += 5.0;
+        if(key == GLUT_KEY_RIGHT) theta2[count-1] -= 5.0;
+        if(key == GLUT_KEY_UP) theta3[count-1] +=5;
+        if(key == GLUT_KEY_DOWN) theta3[count-1] -= 5.0; 
+    } else if (sphere_count > 0) {
+        if(key == GLUT_KEY_LEFT) theta2[sphere_count-1] += 5.0;
+        if(key == GLUT_KEY_RIGHT) theta2[sphere_count-1] -= 5.0;
+        if(key == GLUT_KEY_UP) theta3[sphere_count-1] +=5;
+        if(key == GLUT_KEY_DOWN) theta3[sphere_count-1] -= 5.0;
+    }
+	glutPostRedisplay();
+}
+
+void MyReshape(GLsizei w, GLsizei h)
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	//glOrtho(-2.0,2.0,-2.0,2.0,0.3,5.0);
+	glFrustum(-1.0,1.0,-1.0,1.0,8.0, 10.0);
+	// gluPerspective(60,1.0,0.3,10.0);
+	glViewport(0,0,w,h);
+	return;
+}
+
 void main(int argc, char **argv) {
     glutInit(&argc, argv);
 	glutInitWindowSize(500,500);
-	glutInitWindowPosition(500,200);
+	glutInitWindowPosition(0,65);
     glutCreateWindow("Sculpture Tool");
 
     glutCreateMenu(myMenu);
-    glutAddMenuEntry("Add Cube\t", MENU_CUBE);
-    glutAddMenuEntry("Add Sphere\t", MENU_SPHERE);
+    glutAddMenuEntry("Cube Mode (Left-Click-Spawn)\t", MENU_CUBE);
+    glutAddMenuEntry("Sphere Mode (Left-Click-Spawn)\t", MENU_SPHERE);
     glutAttachMenu(GLUT_RIGHT_BUTTON);
 
+    glutDisplayFunc(display);
+	glutReshapeFunc(MyReshape);
     glutMouseFunc(mouseFunc);
-
+	glutSpecialFunc(rotate);
     glutKeyboardFunc(keyboardFunc);
 	
-    glutDisplayFunc(display);
     glutMainLoop();
 }
